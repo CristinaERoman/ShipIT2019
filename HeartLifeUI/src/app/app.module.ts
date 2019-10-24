a6505c5f17ff03cd3e5e65458e88207271da7c7f
CristinaRoman <cristina.e.roman@gmail.com>
2019-10-24 18:49:07 +0300

    userprofile

--- HeartLifeUI/src/app/app.module.ts
+++ HeartLifeUI/src/app/app.module.ts
@@ -7,17 +7,27 @@ import { RouterModule, Routes } from "@angular/router";
 import { ContactComponent } from "./components/contact/contact.component";
 import { HomeComponent } from "./components/home/home.component";
 import { ProfileComponent } from "./components/profile/profile.component";
+import { UserprofileComponent } from "./components/userprofile/userprofile.component";
 
 const appRoutes: Routes = [
   { path: "home", component: HomeComponent },
   { path: "profile", component: ProfileComponent },
+  { path: "userprofile", component: UserprofileComponent },
   { path: "", redirectTo: "home", pathMatch: "full" },
   { path: "about", component: AboutComponent },
   { path: "contact", component: ContactComponent },
 ];
 
 @NgModule({
-  declarations: [AppComponent, ProfileComponent, AboutComponent, ContactComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    ProfileComponent,
+    UserprofileComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent,
+    UserprofileComponent,
+  ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(
