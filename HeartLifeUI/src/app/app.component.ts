162250b9748ce694f686cad9032d1d8ce2576717
Andrei Smitko <Andrei.Smitko@cerner.com>
2019-10-24 13:52:21 +0300

    Added modal for login.

--- HeartLifeUI/src/app/app.component.ts
+++ HeartLifeUI/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'HeartLifeUI';
+  isLogInModalActive: boolean = false;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  toggleLogInModal() {
+    this.isLogInModalActive = !this.isLogInModalActive;
+  }
+  title = "HeartLifeUI";
 }
